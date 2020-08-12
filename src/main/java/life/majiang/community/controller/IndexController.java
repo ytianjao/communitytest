bc27400f0e360cd71c68a8cbb8048dd4697a167d
qiao <1064509110@qq.com>
2020-08-12 18:35:57 +0800

    展示表单

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,33 +1,45 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuesitonMapper;
 import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
+import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private UserMapper userMapper;
-    @GetMapping("/")
-    public String index(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-            }
-        }
 
+    @Autowired
+    private QuestionService questionService;
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+
+        List<QuestionDTO> questionDTOList = questionService.list();
+        model.addAttribute("questions", questionDTOList);
         return "index";
     }
 }
---
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -0,0 +1,33 @@
+package life.majiang.community.service;
+
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuesitonMapper;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private QuesitonMapper quesitonMapper;
+    public List<QuestionDTO> list() {
+        List<Question> questionList = quesitonMapper.list();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questionList) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}

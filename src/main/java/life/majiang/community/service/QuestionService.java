8cf0b72b794435d2d222faded904e7f6d0aee180
qiao <1064509110@qq.com>
2020-08-18 09:53:51 +0800

    profile

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -21,16 +21,59 @@ public class QuestionService {
     private QuesitonMapper quesitonMapper;
     public PaginationDTO list(Integer page, Integer size) {
         PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalPage;
         Integer totalCount = quesitonMapper.count();
-        paginationDTO.setPagination(totalCount, page, size);
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
         if (page < 1){
             page = 1;
         }
-        if (page >= paginationDTO.getTotalPage()){
-            page = paginationDTO.getTotalPage();
+        if (page >= totalPage){
+            page = totalPage;
         }
+        paginationDTO.setPagination(totalPage, page);
         Integer offset = size*(page - 1);
         List<Question> questionList = quesitonMapper.list(offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questionList) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+
+
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalPage;
+        Integer totalCount = quesitonMapper.countByUserId(userId);
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1){
+            page = 1;
+        }
+        if (page >= totalPage){
+            page = totalPage;
+        }
+        paginationDTO.setPagination(totalPage, page);
+        Integer offset = size*(page - 1);
+        List<Question> questionList = quesitonMapper.listByUserId(userId,offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questionList) {
